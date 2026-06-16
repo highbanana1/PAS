99c82f3753bb4af6aac05140836a126cb9c992c3
GordonS <gordonsun90574@outlook.com>
2026-06-16 17:20:04 +0800

    jump

--- scene/script/maincharac.gd
+++ scene/script/maincharac.gd
@@ -4,21 +4,35 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var jump_count = 1
+
+func _ready() -> void:
+	pass
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	#重制跳跃次数
+	if is_on_floor() and jump_count == 0:
+		jump_count = 1
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count != 0:
 		velocity.y = JUMP_VELOCITY
+		jump_count -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("right", "left")
 	if direction:
 		velocity.x = direction * SPEED
+
+		# 根据移动方向翻转精灵朝向
+		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
