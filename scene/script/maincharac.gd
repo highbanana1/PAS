1ec65773fa415aaf070ba325d30b3d80d0cde114
highbanana1 <assdisj@163.com>
2026-06-16 17:57:56 +0800

    dash

--- scene/script/maincharac.gd
+++ scene/script/maincharac.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-
+const DASH_AMT: float= 360.0
+const DASH_TIME: float= 0.16
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var can_dash: bool = true
+var is_dashing: bool=false
+var dash_dir: Vector2= Vector2.RIGHT
+var dash_timer: float=0.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var jump_count = 1
@@ -12,14 +16,47 @@ func _ready() -> void:
 	pass
 
 
+func _dash_logic(delta: float)-> void:
+	var input_dir: Vector2 = Vector2(
+		Input.get_axis("left","right"),
+		Input.get_axis("up","down")
+	).normalized()
+	
+	if input_dir.x != 0:
+		dash_dir.x = input_dir.x
+		
+	if can_dash and Input.is_action_just_pressed("dash"):
+		var final_dash_dir: Vector2 = dash_dir
+		if input_dir.y !=0 and input_dir.x ==0:
+			final_dash_dir.x=0
+		final_dash_dir.y=input_dir.y
+		
+		can_dash = false
+		is_dashing = true
+		dash_timer = DASH_TIME
+		
+		velocity = final_dash_dir * DASH_AMT
+	
+	if is_dashing:
+		
+		dash_timer -=delta
+		if dash_timer <= 0.0:
+			is_dashing= false
+		
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if !is_dashing:
+			velocity += get_gravity() * delta
 
 	#重制跳跃次数
 	if is_on_floor() and jump_count == 0:
 		jump_count = 1
+	if is_on_floor():
+		if !is_dashing and !can_dash:
+			can_dash= true
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and jump_count != 0:
 		velocity.y = JUMP_VELOCITY
@@ -27,13 +64,14 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("right", "left")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 
 		# 根据移动方向翻转精灵朝向
 		animated_sprite.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if !is_dashing:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	_dash_logic(delta)
 	move_and_slide()
